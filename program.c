cf6ed4809bd0bcf1a09ed145a809df1226b40057
Coockie1173 <rudjerud@hotmail.com>
2023-06-27 22:24:45 +0200

    proof of concept

--- program.c
+++ program.c
@@ -11,6 +11,19 @@ void Init();
 void Update();
 void Death();
 
+int CoinPickedUp = 0xFFFFFFFF;
+float CoinTimer = 0xFFFFFFFF;
+float PreviousVelocity = 0xFFFFFFFF;
+
+float CoinOrigX = 0xFFFFFFFF;
+float CoinOrigY = 0xFFFFFFFF;
+float CoinOrigZ = 0xFFFFFFFF;
+float CoinOrigRotX = 0xFFFFFFFF;
+float CoinOrigRotY = 0xFFFFFFFF;
+float CoinOrigRotZ = 0xFFFFFFFF;
+
+SkyBox *MySkybox = 0xFFFFFFFF;
+
 AllocatedObject ObjectTable[] =
     {
         {0, 0, 0, 0, 0},
@@ -53,9 +66,9 @@ SpecialObject SpecialObjectTable[] =
     {
         {
             0, // ptr should always be 0, there's a function that sets it
-            0x9D,
+            64,
             2350.0f,
-            200.0f,
+            240.0f,
             2850.0f,
             0x0F,
             0x01,
@@ -68,10 +81,9 @@ SpecialObject SpecialObjectTable[] =
 
 void *JumpTable[] =
     {
-        &DoNothing, //exit scene
-        &Init, //init
-        &Update, //update
-        &DoNothing, 
+        &DoNothing, // exit scene
+        &Init,      // init
+        &Update,    // update
         &DoNothing,
         &DoNothing,
         &DoNothing,
@@ -83,7 +95,8 @@ void *JumpTable[] =
         &DoNothing,
         &DoNothing,
         &DoNothing,
-        &Death, //death
+        &DoNothing,
+        &Death, // death
         &DoNothing};
 
 const void *__start()
@@ -97,12 +110,12 @@ const void main(int State)
     {
         if (State == 0x52)
         {
-            DoNothing(); //tile EFFECT 1
+            DoNothing(); // tile EFFECT 1
             return;
         }
         else if (State == 0x53)
         {
-            DoNothing(); //tile EFFECT 2
+            DoNothing(); // tile EFFECT 2
             return;
         }
         return;
@@ -163,10 +176,15 @@ void Init()
     for (int i = 0; i < SpecialObjectCount; i++)
     {
         SpecialObject SO = SpecialObjectTable[i];
-        int *ModelPTR = LoadModelInRAM(SO.ModelID, 0xD0, SO.X, SO.Y, SO.Z);
+        ModelEntry *ModelPTR = LoadModelInRAM(SO.ModelID, 0xD0, SO.X, SO.Y, SO.Z);
         LoadModelUnk(ModelPTR, 0x03000000);
         SpecialObjectTable[i].Ptr = ModelPTR;
     }
+
+    MySkybox = SetSkyBox(0x1FD);
+    CoinPickedUp = 0;
+    CoinTimer = 0;
+    PreviousVelocity = 0;
     return;
 }
 
@@ -175,7 +193,79 @@ void Death()
     return;
 }
 
+int isPlayerWithinRange(ModelData mdl, ModelData playerMDL, float range)
+{
+    // Calculate the distance between the two positions
+    float distanceX = mdl.posX - playerMDL.posX;
+    float distanceZ = mdl.posZ - playerMDL.posZ;
+    float distanceSquared = (distanceX * distanceX) + (distanceZ * distanceZ);
+
+    // Calculate the squared range to avoid using sqrt function
+    float rangeSquared = range * range;
+
+    // Compare the squared distance with the squared range
+    if (distanceSquared <= rangeSquared)
+    {
+        return 1; // Player is within range
+    }
+    else
+    {
+        return 0; // Player is not within range
+    }
+}
+
 void Update()
 {
+    ModelData *mdl = SpecialObjectTable[0].Ptr->ModelDataPtr;
+    ModelData *playerMDL = (*PlayerPointer)->ModelDataPtr;
 
+    if (CoinPickedUp == 1)
+    {
+        ModelEntry *PP = *(PlayerPointer);
+
+        mdl->posX = playerMDL->posX;
+        mdl->posY = playerMDL->posY;
+        mdl->posY += 200;
+        mdl->posZ = playerMDL->posZ;
+        mdl->rotX = playerMDL->rotX;
+        mdl->rotY = playerMDL->rotY;
+        mdl->rotZ = playerMDL->rotZ;
+
+        CoinTimer -= *DeltaTime;
+        if (CoinTimer < 0)
+        {
+            CoinPickedUp = 2;
+        }
+        if (((*PlayerInput) & L_TRIG) != 0 && PP->VelocityY == 0 && PreviousVelocity <= 0)
+        {
+            PP->VelocityY = -20;
+        }
+        PreviousVelocity = PP->VelocityY;
+    }
+    else if (CoinPickedUp == 2)
+    {
+        mdl->posX = CoinOrigX;
+        mdl->posY = CoinOrigY;
+        mdl->posZ = CoinOrigZ;
+        mdl->rotX = CoinOrigRotX;
+        mdl->rotY = CoinOrigRotY;
+        mdl->rotZ = CoinOrigRotZ;
+        CoinPickedUp = 0;
+    }
+    else
+    {
+        mdl->rotY += *DeltaTime;
+        if (isPlayerWithinRange(*mdl, *playerMDL, 25))
+        {
+            CoinPickedUp = 1;
+            CoinTimer = 30 * 60.0f;
+            PreviousVelocity = (*PlayerPointer)->VelocityY;
+            CoinOrigX = mdl->posX;
+            CoinOrigY = mdl->posY;
+            CoinOrigZ = mdl->posZ;
+            CoinOrigRotX = mdl->rotX;
+            CoinOrigRotY = mdl->rotY;
+            CoinOrigRotZ = mdl->rotZ;
+        }
+    }
 }
